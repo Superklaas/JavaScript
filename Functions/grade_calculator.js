49ec8c3db8410deb750d373d8626a7a361829f6e
Klaas Decock <klaasdc@icloud.com>
2020-09-16 17:49:27 +0200

    commit

--- Functions/grade_calculator.js
+++ Functions/grade_calculator.js
@@ -1,15 +1,23 @@
-let score = 100
-let total = 100
-let percent = (score/total)*100
 
-if(percent>=90) {
-    console.log(`You  got an A (${Math.round(percent)}%)`)
-} else if (percent<90 && percent>=80) {
-    console.log(`You  got a B (${Math.round(percent)}%)`)
-} else if (percent<80 && percent>=70) {
-      console.log(`You  got a C (${Math.round(percent)}%)`)
-} else if (percent<70 && percent>=60) {
-      console.log(`You  got a D (${Math.round(percent)}%)`)
-} else if (percent<60) {
-      console.log(`You  got an F (${Math.round(percent)}%)`)
+function gradeCalculator(score,total) {
+    let percent = (score/total)*100
+    let grade
+    if(percent>=90) {
+        grade = "A"
+    } else if (percent<90 && percent>=80) {
+        grade = "B"
+    } else if (percent<80 && percent>=70) {
+        grade = "C"
+    } else if (percent<70 && percent>=60) {
+        grade = "D"
+    } else if (percent<60) {
+        grade = "F"
+    }
+    return `Your grade is ${grade} (${Math.round(percent)}%)`
 }
+
+let result = gradeCalculator(89,119)
+console.log(result)
+
+
+
