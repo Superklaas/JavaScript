bb8a983f65a7ef1f134bbd89ae31d49336cc52d7
Klaas Decock <klaasdc@icloud.com>
2020-09-16 21:34:29 +0200

    commit

---
+++ Objects/expense_tracker.js
@@ -0,0 +1,33 @@
+let myAccount = {
+    name: 'Klaas',
+    expense: 0,
+    income: 0,
+}
+
+let addExpense = function(account,amount) {
+    account.expense += amount;
+}
+
+let addIncome = function(account,amount) {
+    account.income += amount;
+}
+
+let resetAccount = function(account) {
+    account.expense = 0;
+    account.income = 0;
+}
+
+let getAccountSummary = function(account) {
+    let balance = account.income - account.expense;
+    let accountSummary = `The account for ${account.name} has $${balance}. Income: $${account.income}. Expenses: $${account.expense}.`
+    console.log(accountSummary)
+}
+
+addIncome(myAccount,2000)
+addExpense(myAccount,100)
+addExpense(myAccount,260)
+getAccountSummary(myAccount)
+resetAccount(myAccount)
+getAccountSummary(myAccount)
+
+
